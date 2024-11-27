0a8a36d9e42a0a5446096136c6abb1764ab04aad
Yuri Barsotti <yuribarsotti@gmail.com>
2024-11-26 22:50:45 -0300

    change structure

---
+++ lua/ybarsotti/lazy-plugins.lua
@@ -0,0 +1,7 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+require('lazy').setup("ybarsotti.plugins")
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/ybarsotti/plugins/sleuth.lua
@@ -0,0 +1,6 @@
+return {
+  {
+    'tpope/vim-sleuth',
+    opts = {},
+  },
+}

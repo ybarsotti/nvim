e6d1c426403b23d6f231ca0bde59bdadefc20c5f
Yuri Barsotti <yuribarsotti@gmail.com>
2024-11-28 18:13:47 -0300

    move small plugins to misc

---
+++ lua/ybarsotti/plugins/misc.lua
@@ -0,0 +1,30 @@
+-- Standalone plugins with less than 10 lines of configuration
+return {
+  { -- Autoclose parentheses, brackets, quotes, etc.
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+  {
+    -- Tmux & split window navigation
+    'christoomey/vim-tmux-navigator',
+  },
+  {
+    -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',
+  },
+  { -- Highlight TODO, FIXME etc
+    'folke/todo-comments.nvim'
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+}

c00e65226893d1c741db635f3dba38369c90ae9f
Yuri Barsotti <yuribarsotti@gmail.com>
2024-11-28 18:02:14 -0300

    configure indent line; remove conform

--- lua/ybarsotti/plugins/indent_line.lua
+++ lua/ybarsotti/plugins/indent_line.lua
@@ -1,9 +1,25 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts = {
+    indent = {
+      char = '▏',
+    },
+    scope = {
+      show_start = false,
+      show_end = false,
+      show_exact_scope = false,
+    },
+    exclude = {
+      filetypes = {
+        'help',
+        'startify',
+        'dashboard',
+        'packer',
+        'neogitstatus',
+        'NvimTree',
+        'Trouble',
+      },
+    },
   },
 }

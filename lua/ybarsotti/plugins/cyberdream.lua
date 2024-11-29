b8d1e5eb766cf76f14323753f449c6a4dadd570b
Yuri Barsotti <yuribarsotti@gmail.com>
2024-11-28 22:52:49 -0300

    change tokyonight for cyberdream

---
+++ lua/ybarsotti/plugins/cyberdream.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('cyberdream').setup {
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+      }
+      vim.cmd 'colorscheme cyberdream'
+    end,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et

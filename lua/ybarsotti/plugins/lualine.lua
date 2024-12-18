0d59f8d900505bda944943c7553d7009b98e3daa
Yuri Barsotti Mendes <yuribarsotti@gmail.com>
2024-12-18 10:45:47 -0300

    lualine theme

--- lua/ybarsotti/plugins/lualine.lua
+++ lua/ybarsotti/plugins/lualine.lua
@@ -40,7 +40,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'nord', -- Set theme based on environment variable
+        theme = 'auto', -- Set theme based on environment variable
         -- Some useful glyphs:
         -- https://www.nerdfonts.com/cheat-sheet
         --        

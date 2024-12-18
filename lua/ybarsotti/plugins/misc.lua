fd1055d8417e23d6413562fe0be7f2cf7a805f4f
Yuri Barsotti Mendes <yuribarsotti@gmail.com>
2024-12-18 11:31:53 -0300

    add codesnap

--- lua/ybarsotti/plugins/misc.lua
+++ lua/ybarsotti/plugins/misc.lua
@@ -47,4 +47,18 @@ return {
   { -- Allows to repeat non native commands
     'tpope/vim-repeat',
   },
+  {
+    'mistricky/codesnap.nvim',
+    build = 'make build_generator',
+    keys = {
+      { '<leader>cc', '<cmd>CodeSnap<cr>', mode = 'x', desc = 'Save selected code snapshot into clipboard' },
+      { '<leader>cs', '<cmd>CodeSnapSave<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
+    },
+    opts = {
+      save_path = '~/Pictures',
+      has_breadcrumbs = true,
+      bg_theme = 'grape',
+      watermark = ""
+    },
+  },
 }

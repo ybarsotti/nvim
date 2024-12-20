c638adeafd357fe2a7ee57b09341410ee91153ca
Yuri Barsotti Mendes <yuribarsotti@gmail.com>
2024-12-20 10:06:40 -0300

    add python debugger

--- lua/ybarsotti/init.lua
+++ lua/ybarsotti/init.lua
@@ -19,5 +19,8 @@ require 'ybarsotti.lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'ybarsotti.lazy-plugins'
 
+-- Prepend mise shims to PATH
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/ybarsotti/plugins/test.lua
+++ lua/ybarsotti/plugins/test.lua
@@ -94,36 +94,51 @@ return {
   },
   { -- Debugger
     'mfussenegger/nvim-dap',
-    optional = true,
     -- stylua: ignore
-    keys = {
-      { 
-        "<leader>td",
-        function() 
-          require("neotest").run.run({strategy = "dap"})
-        end,
-        desc = "Debug Nearest"
-      },
-      {
-        "<leader>dO",
-        function()
-          require("dap").step_out()
-        end,
-        desc = "Step Out",
-      },
-      {
-        "<leader>do",
-        function()
-          require("dap").step_over()
-        end,
-        desc = "Step Over",
-      },
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap-python"
     },
-  },
-  { -- Debugger extension
-    'theHamsta/nvim-dap-virtual-text',
-    opts = {
-      virt_text_win_col = 80,
-    },
+    config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
+
+      require('dapui').setup()
+      require('dap-go').setup()
+      require('nvim-dap-virtual-text').setup {}
+      require('dap-python').setup('python3')
+
+      -- Eval var under cursor
+      vim.keymap.set('n', '<space>?', function()
+        require('dapui').eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
+      vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
+
+      vim.keymap.set('n', '<F1>', dap.continue)
+      vim.keymap.set('n', '<F2>', dap.step_into)
+      vim.keymap.set('n', '<F3>', dap.step_over)
+      vim.keymap.set('n', '<F4>', dap.step_out)
+      vim.keymap.set('n', '<F5>', dap.step_back)
+      vim.keymap.set('n', '<F13>', dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
   },
 }

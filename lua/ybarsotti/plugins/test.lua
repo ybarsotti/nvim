0bb0bab978cba0a0641a68fc8252d4e8de2d4992
Yuri Barsotti Mendes <yuribarsotti@gmail.com>
2025-03-14 13:15:05 -0300

    configure debugger for javascript

--- lua/ybarsotti/plugins/test.lua
+++ lua/ybarsotti/plugins/test.lua
@@ -91,6 +91,11 @@ return {
       },
     },
   },
+  {
+    'microsoft/vscode-js-debug',
+    opt = true,
+    build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
+  },
   { -- Debugger
     'mfussenegger/nvim-dap',
     -- stylua: ignore
@@ -101,13 +106,14 @@ return {
       "nvim-neotest/nvim-nio",
       "williamboman/mason.nvim",
       "mfussenegger/nvim-dap-python",
-      "mfussenegger/nvim-dap-vscode-js",
+      "mxsdev/nvim-dap-vscode-js",
     },
     config = function()
       local dap = require 'dap'
       local ui = require 'dapui'
+      local dap_vscode = require 'dap-vscode-js'
 
-      require('dapui').setup()
+      ui.setup()
       require('dap-go').setup {
         dap_configurations = {
           {
@@ -124,20 +130,68 @@ return {
       require('nvim-dap-virtual-text').setup {}
       require('dap-python').setup 'python3'
 
+      dap_vscode.setup {
+        debugger_path = os.getenv 'HOME' .. '/.local/share/nvim/vscode-js-debug',
+        adapters = { 'pwa-node', 'pwa-chrome' },
+      }
+
+      for _, language in ipairs { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' } do
+        dap.configurations[language] = {
+          {
+            type = 'pwa-chrome',
+            request = 'attach',
+            name = 'Chrome Inspector',
+            program = '${file}',
+            cwd = vim.fn.getcwd(),
+            sourceMaps = true,
+            protocol = 'inspector',
+            port = 9222,
+            webRoot = '${workspaceFolder}',
+          },
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch file',
+            program = '${file}',
+            cwd = '${workspaceFolder}',
+          },
+          {
+            type = 'pwa-node',
+            request = 'attach',
+            name = 'Attach',
+            processId = require('dap.utils').pick_process,
+            cwd = '${workspaceFolder}',
+          },
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Debug Jest Tests',
+            -- trace = true, -- include debugger info
+            runtimeExecutable = 'node',
+            runtimeArgs = {
+              './node_modules/jest/bin/jest.js',
+              '--runInBand',
+            },
+            rootPath = '${workspaceFolder}',
+            cwd = '${workspaceFolder}',
+            console = 'integratedTerminal',
+            internalConsoleOptions = 'neverOpen',
+          },
+        }
+      end
       -- Eval var under cursor
-      vim.keymap.set('n', '<space>t?', function()
+      vim.keymap.set('n', '<leader>t?', function()
         require('dapui').eval(nil, { enter = true })
-      end, { desc = "DAP: Eval under cursor" })
-
-      vim.keymap.set('n', '<space>tb', dap.toggle_breakpoint, { desc = "DAP: [T]oggle [B]reakpoint" })
-      vim.keymap.set('n', '<space>tc', dap.run_to_cursor, { desc = "DAP: Run [C]ursor" })
-
-      vim.keymap.set('n', '<F1>', dap.continue, { desc = "DAP: Continue" })
-      vim.keymap.set('n', '<F2>', dap.step_into, { desc = "DAP: Step Into" })
-      vim.keymap.set('n', '<F3>', dap.step_over, { desc = "DAP: Step Over" })
-      vim.keymap.set('n', '<F4>', dap.step_out, { desc = "DAP: Step Out" })
-      vim.keymap.set('n', '<F5>', dap.step_back, { desc = "DAP: Step Back" })
-      vim.keymap.set('n', '<F13>', dap.restart, { desc = "DAP: Restart" })
+      end, { desc = 'DAP: Eval under cursor' })
+      vim.keymap.set('n', '<leader>tb', dap.toggle_breakpoint, { desc = 'DAP: [T]oggle [B]reakpoint' })
+      vim.keymap.set('n', '<leader>tc', dap.run_to_cursor, { desc = 'DAP: Run [C]ursor' })
+      vim.keymap.set('n', '<leader>tu', ui.toggle, { desc = 'DAP: [T]oggle [U]i' })
+      vim.keymap.set('n', '<F1>', dap.continue, { desc = 'DAP: Continue' })
+      vim.keymap.set('n', '<F2>', dap.step_into, { desc = 'DAP: Step Into' })
+      vim.keymap.set('n', '<F3>', dap.step_over, { desc = 'DAP: Step Over' })
+      vim.keymap.set('n', '<F4>', dap.step_out, { desc = 'DAP: Step Out' })
+      vim.keymap.set('n', '<F5>', dap.step_back, { desc = 'DAP: Step Back' })
+      vim.keymap.set('n', '<F13>', dap.restart, { desc = 'DAP: Restart' })
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()

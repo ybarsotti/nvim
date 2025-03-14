e1d7d261006fe2e55796fa26f0cd44acf42a6ed9
Yuri Barsotti Mendes <yuribarsotti@gmail.com>
2025-03-14 11:04:17 -0300

    update codesnap build command

--- lua/ybarsotti/plugins/misc.lua
+++ lua/ybarsotti/plugins/misc.lua
@@ -49,7 +49,7 @@ return {
   },
   { -- Code screenshots
     'mistricky/codesnap.nvim',
-    build = 'make build_generator',
+    build = 'make',
     keys = {
       { '<leader>cs', '<Esc><cmd>CodeSnap<cr>', mode = 'x', desc = 'Codesnap: Save selected code snapshot into clipboard' },
       { '<leader>cS', '<Esc><cmd>CodeSnapSave<cr>', mode = 'x', desc = 'Codesnap: Save selected code snapshot in ~/Pictures' },

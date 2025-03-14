9a65f5ccb4bcf0295445f1c88383d32349d7d977
Yuri Barsotti Mendes <yuribarsotti@gmail.com>
2025-03-13 22:06:31 -0300

    change format keybind to "cf"

--- lua/ybarsotti/plugins/autoformatting.lua
+++ lua/ybarsotti/plugins/autoformatting.lua
@@ -24,13 +24,13 @@ return {
       },
     }
 
-    vim.keymap.set({ 'n', 'v' }, '<leader>ff', function()
+    vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
       conform.format {
         lsp_fallback = true,
         async = false,
         timeout_ms = 1000,
       }
-    end, { desc = 'Format file or range (in visual mode) - (Conform)' })
+    end, { desc = 'Conform: [C]ode [F]ormat file or range (in visual mode)' })
   end,
 }
 

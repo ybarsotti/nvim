407a9cd0dbc561eabc0e809b281292986d877b72
Yuri Barsotti Mendes <yuribarsotti@gmail.com>
2025-03-17 12:48:50 -0300

    update debugger

--- lua/ybarsotti/plugins/test.lua
+++ lua/ybarsotti/plugins/test.lua
@@ -91,18 +91,18 @@ return {
       },
     },
   },
-  {
-    'microsoft/vscode-js-debug',
-    opt = true,
-    build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
-  },
   { -- Debugger
     'mfussenegger/nvim-dap',
     -- stylua: ignore
     dependencies = {
       "leoluz/nvim-dap-go",
       "rcarriga/nvim-dap-ui",
-      "theHamsta/nvim-dap-virtual-text",
+      {"theHamsta/nvim-dap-virtual-text", opts = {}},
+      {
+        'microsoft/vscode-js-debug',
+        -- Run cd ~/.local/share/nvim/lazy/vscode-js-debug && rm -rf package-lock.json if this fails
+        build = 'npm i && npm run compile vsDebugServerBundle && rm -rf out && mv -f dist out',
+      },
       "nvim-neotest/nvim-nio",
       "williamboman/mason.nvim",
       "mfussenegger/nvim-dap-python",
@@ -110,10 +110,12 @@ return {
     },
     config = function()
       local dap = require 'dap'
-      local ui = require 'dapui'
-      local dap_vscode = require 'dap-vscode-js'
-
-      ui.setup()
+      require('dap-vscode-js').setup {
+        debugger_path = vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug',
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+      }
+      require('nvim-dap-virtual-text').setup {}
+      require('dap-python').setup 'python3'
       require('dap-go').setup {
         dap_configurations = {
           {
@@ -127,84 +129,190 @@ return {
           port = '40000',
         },
       }
-      require('nvim-dap-virtual-text').setup {}
-      require('dap-python').setup 'python3'
 
-      dap_vscode.setup {
-        debugger_path = os.getenv 'HOME' .. '/.local/share/nvim/vscode-js-debug',
-        adapters = { 'pwa-node', 'pwa-chrome' },
-      }
+      if not dap.adapters['node'] then
+        dap.adapters['node'] = function(cb, config)
+          if config.type == 'node' then
+            config.type = 'pwa-node'
+          end
+          local nativeAdapter = dap.adapters['pwa-node']
+          if type(nativeAdapter) == 'function' then
+            nativeAdapter(cb, config)
+          else
+            cb(nativeAdapter)
+          end
+        end
+      end
 
-      for _, language in ipairs { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' } do
-        dap.configurations[language] = {
-          {
-            type = 'pwa-chrome',
-            request = 'attach',
-            name = 'Chrome Inspector',
-            program = '${file}',
-            cwd = vim.fn.getcwd(),
-            sourceMaps = true,
-            protocol = 'inspector',
-            port = 9222,
-            webRoot = '${workspaceFolder}',
+      if not dap.adapters['pwa-node'] then
+        dap.adapters['pwa-node'] = {
+          type = 'server',
+          host = 'localhost',
+          port = '${port}',
+          executable = {
+            command = 'node',
+            args = {
+              os.getenv 'HOME' .. '/.local/share/nvim/vscode-js-debug/out/src/vsDebugServer.js',
+              '${port}',
+            },
           },
+        }
+      end
+
+      local dap_vscode = require 'dap.ext.vscode'
+      local js_filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' }
+      local json = require 'plenary.json'
+      ---@diagnostic disable-next-line: duplicate-set-field
+      dap_vscode.json_decode = function(str)
+        return vim.json.decode(json.json_strip_comments(str, {}))
+      end
+      dap_vscode.type_to_filetypes['node'] = js_filetypes
+
+      for _, language in ipairs(js_filetypes) do
+        dap.configurations[language] = {
+          -- Debug single nodejs files
           {
+            name = 'Launch file',
             type = 'pwa-node',
             request = 'launch',
-            name = 'Launch file',
             program = '${file}',
             cwd = '${workspaceFolder}',
+            args = { '${file}' },
+            sourceMaps = true,
+            sourceMapPathOverrides = {
+              ['./*'] = '${workspaceFolder}/src/*',
+            },
           },
+          -- Debug nodejs processes (make sure to add --inspect when you run the process)
           {
+            name = 'Attach',
             type = 'pwa-node',
             request = 'attach',
-            name = 'Attach',
             processId = require('dap.utils').pick_process,
             cwd = '${workspaceFolder}',
+            sourceMaps = true,
           },
           {
+            name = 'Debug Jest Tests',
             type = 'pwa-node',
             request = 'launch',
-            name = 'Debug Jest Tests',
-            -- trace = true, -- include debugger info
             runtimeExecutable = 'node',
-            runtimeArgs = {
-              './node_modules/jest/bin/jest.js',
-              '--runInBand',
-            },
+            runtimeArgs = { '${workspaceFolder}/node_modules/.bin/jest', '--runInBand' },
             rootPath = '${workspaceFolder}',
             cwd = '${workspaceFolder}',
             console = 'integratedTerminal',
             internalConsoleOptions = 'neverOpen',
+            -- args = {'${file}', '--coverage', 'false'},
+            -- sourceMaps = true,
+            -- skipFiles = {'node_internals/**', 'node_modules/**'},
+          },
+          {
+            name = 'Debug Vitest Tests',
+            type = 'pwa-node',
+            request = 'launch',
+            cwd = vim.fn.getcwd(),
+            program = '${workspaceFolder}/node_modules/vitest/vitest.mjs',
+            args = { 'run', '${file}' },
+            autoAttachChildProcesses = true,
+            smartStep = true,
+            skipFiles = { '<node_internals>/**', 'node_modules/**' },
+          },
+          -- Debug web applications (client side)
+          {
+            name = 'Launch & Debug Chrome',
+            type = 'pwa-chrome',
+            request = 'launch',
+            url = function()
+              local co = coroutine.running()
+              return coroutine.create(function()
+                vim.ui.input({ prompt = 'Enter URL: ', default = 'http://localhost:3000' }, function(url)
+                  if url == nil or url == '' then
+                    return
+                  else
+                    coroutine.resume(co, url)
+                  end
+                end)
+              end)
+            end,
+            webRoot = vim.fn.getcwd(),
+            protocol = 'inspector',
+            sourceMaps = true,
+            userDataDir = false,
+            resolveSourceMapLocations = {
+              '${workspaceFolder}/**',
+              '!**/node_modules/**',
+            },
+
+            -- From https://github.com/lukas-reineke/dotfiles/blob/master/vim/lua/plugins/dap.lua
+            -- To test how it behaves
+            rootPath = '${workspaceFolder}',
+            cwd = '${workspaceFolder}',
+            console = 'integratedTerminal',
+            internalConsoleOptions = 'neverOpen',
+            sourceMapPathOverrides = {
+              ['./*'] = '${workspaceFolder}/src/*',
+            },
+          },
+          {
+            name = '----- ↑ launch.json configs (if available) ↑ -----',
+            type = '',
+            request = 'launch',
           },
         }
       end
-      -- Eval var under cursor
+
+      require('dapui').setup {
+        floating = { border = 'single' },
+        layouts = {
+          {
+            elements = {
+              { id = 'scopes', size = 0.25 },
+              { id = 'breakpoints', size = 0.25 },
+              { id = 'stacks', size = 0.25 },
+              { id = 'watches', size = 0.25 },
+            },
+            position = 'left',
+            size = 40,
+          },
+          {
+            elements = {
+              { id = 'console', size = 0.6 },
+              { id = 'repl', size = 0.4 },
+            },
+            position = 'bottom',
+            size = 10,
+          },
+        },
+      }
+      local dapui = require 'dapui'
       vim.keymap.set('n', '<leader>t?', function()
         require('dapui').eval(nil, { enter = true })
       end, { desc = 'DAP: Eval under cursor' })
-      vim.keymap.set('n', '<leader>tb', dap.toggle_breakpoint, { desc = 'DAP: [T]oggle [B]reakpoint' })
-      vim.keymap.set('n', '<leader>tc', dap.run_to_cursor, { desc = 'DAP: Run [C]ursor' })
-      vim.keymap.set('n', '<leader>tu', ui.toggle, { desc = 'DAP: [T]oggle [U]i' })
-      vim.keymap.set('n', '<F1>', dap.continue, { desc = 'DAP: Continue' })
-      vim.keymap.set('n', '<F2>', dap.step_into, { desc = 'DAP: Step Into' })
-      vim.keymap.set('n', '<F3>', dap.step_over, { desc = 'DAP: Step Over' })
-      vim.keymap.set('n', '<F4>', dap.step_out, { desc = 'DAP: Step Out' })
-      vim.keymap.set('n', '<F5>', dap.step_back, { desc = 'DAP: Step Back' })
-      vim.keymap.set('n', '<F13>', dap.restart, { desc = 'DAP: Restart' })
+      vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'DAP: Toggle [B]reakpoint' })
+      vim.keymap.set('n', '<leader>dC', dap.run_to_cursor, { desc = 'DAP: Run [C]ursor' })
+      vim.keymap.set('n', '<leader>dj', dap.down, { desc = 'DAP: Go down' })
+      vim.keymap.set('n', '<leader>dk', dap.up, { desc = 'DAP: Go up' })
+      vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'DAP: Continue' })
+      vim.keymap.set('n', '<leader>dp', dap.pause, { desc = 'DAP: Pause' })
+      vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'DAP: Step Into' })
+      vim.keymap.set('n', '<leader>do', dap.step_over, { desc = 'DAP: Step Over' })
+      vim.keymap.set('n', '<leader>dO', dap.step_out, { desc = 'DAP: Step Out' })
+      vim.keymap.set('n', '<leader>ds', dap.session, { desc = 'DAP: Session' })
+      vim.keymap.set('n', '<leader>dt', dap.terminate, { desc = 'DAP: Terminate' })
+      vim.keymap.set('n', '<leader>dr', dap.repl.toggle, { desc = 'DAP: Toggle REPL' })
+      vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = 'DAP: Toggle [U]i' })
+      vim.keymap.set('n', '<leader>dw', require('dap.ui.widgets').hover, { desc = 'DAP: Widget - Hover' })
+      vim.keymap.set('n', '<leader>td', function()
+        require('neotest').run.run { strategy = 'dap' }
+      end, { desc = 'Neotest: Debug nearest' })
 
-      dap.listeners.before.attach.dapui_config = function()
-        ui.open()
+      vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
+
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open { reset = true }
       end
-      dap.listeners.before.launch.dapui_config = function()
-        ui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        ui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        ui.close()
-      end
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
     end,
   },
 }
